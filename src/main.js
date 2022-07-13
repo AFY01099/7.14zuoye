6b1576490e682240078023daf07d1862de64f4d2
fuyu <afy01099@163.com>
2022-07-13 09:58:24 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+//webpack 打包的入口文件
+import Vue from 'vue'//引入vue 
+import App from './App.vue'//引入根组件
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
+Vue.config.productionTip = false//提示语句
+//new 构造函数
+new Vue({//生成一个实例
+  render: h => h(App),//渲染 //h函数 //createElement 
+  //挂载到App身上
+  //渲染App
 }).$mount('#app')
